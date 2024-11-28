4c3c1920c8ec3b51553ad32c45fe4cd19cc12585
calanche08 <ing.calanche@gmail.com>
2024-11-27 20:50:18 -0400

    correcciones clase anterior

--- app/src/main/java/com/calanche/curso/MainActivity.java
+++ app/src/main/java/com/calanche/curso/MainActivity.java
@@ -22,106 +22,95 @@ public class MainActivity extends AppCompatActivity {
 
         texto.setText("Hola Mundo"); // Qñqdiendo un texto para mostrar
 
+        
+        // instanciando clase Planta
+        
+        Planta girasol = new Planta();
+        
+        girasol.edad = 10;
+        
+        girasol.nombre="pepe";
+        
+        girasol.sexo='M';
+        
+        girasol.tamano=50;
+        
+        girasol.crece();
+        
+        girasol.duerme();
+        
+        girasol.respira();
+        
+        Planta cafe = new Planta();
+        
+        cafe.edad = 50;
+        
+        cafe.nombre="ruffy";
+        
+        cafe.sexo ='M';
+        
+        cafe.tamano =200;
+        
+        
+        cafe.crece();
+        
+        cafe.duerme();
+        
+        cafe.respira();
+        
+        
+        Planta arroz = new Planta();
+        
+        arroz.edad = 300;
+        
+        arroz.nombre="anna";
+        
+        arroz.sexo ='F';
+        
+        arroz.tamano =15;
+        
+        
+        arroz.crece();
+        
+        arroz.duerme();
+        
+        arroz.respira();
+        
+        
         // Añadiendo el.contecto a la aplicación
         setContentView(panatalla);
     }
-}
-//esto es un comentario de una sola linea
-
-/* esto es un comentario de varias lineas
-continua aqui */
-
-/** comentario de varias lineas
-de documentacion */
-
-// clase #1 conceptos de POO
-
-// practica de la clase atributos y metodos
-
-
-//aprendiendo a instancear una clase
-
-Planta girasol = new Planta();
-
-//propiedades o atributos del girasol
-
-girasol.edad= 10;
-girasol.nombre= "Pepe";
-girasol.sexo= 'M';
-girasol.tamano= 50;
-
-// metodos o acciones
-
-girasol.crece();
-girasol.duerme();
-girasol.respira();
-
-// instanceando planta de cafe
-
-Planta cafe = new Planta();
-
-//atributos del cafe
-
-cafe.edad= 50;
-cafe.nombre="Ruffy";
-cafe.sexo='M';
-cafe.tamano=200;
-
-// metodos del cafe
-
-cafe.duerme();
-cafe.respira();
-cafe.duerme();
-
-Planta arroz = new Planta();
-
-arroz.edad= 300;
-arroz.nombre="Ana";
-arroz.sexo='F';
-cafe.tamano= 15;
-
-
-// metodos del arroz
-
-arroz.crece();
-arroz.respira();
-arroz.duerme();
-
-
-class Planta {   // clase planta
     
-    /* atributos de la clase planta los 
-    cuales se generan con la finalidad de
-    entender y memorizar los conceptos */
-    
-    int edad =12;
-    int tamano =20;
-    string nombre ="maiz";
-    char sexo ='f';
-    
-    
-    /** metodos de la clase planta
-    -respira=metodo que mantiene con vida
-    -crece=metodo que hace cambiante
-    -duerme=metodo que desarrolla */
-    
-    void respira()
-    {
+    class Planta {   // nueva clase Planta
+        
+        //atributos de clase planta
+        
+        int edad =12;
+        int tamano = 20;
+        String nombre ="maiz";
+        char sexo ='F';
+        
+        
+        //metodos de la clase planta
+        
+        void respira()
+        {}
+        
+        void crece()
+        {}
+        
+        void duerme()
+        {}
+        
         
     }
     
-    void crece()
-    {
-        
-    }
     
-    void duerme()
-    {
-        
-    }
+    
     
     
     
     
     
 }
+
